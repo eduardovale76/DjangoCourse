39c71350120e41786b1f19508aeedadcad3d140a
eduardovale76 <eduardovale76@gmail.com>
2021-02-27 17:17:56 -0300

    Bloco de Templates

--- pypro/aperitivos/test_video.py
+++ pypro/aperitivos/test_video.py
@@ -10,7 +10,7 @@ def test_status_code(resp):
     assert resp.status_code==200
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+    assert_contains(resp, 'Video Aperitivo: Motivação</h1>')
 
 def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/25122"')
+    assert_contains(resp, 'src="https://www.youtube.com/embed/JkGbB0XtH1w"')
