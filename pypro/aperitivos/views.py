eaa0a94583bbe14c050f1deea155d48f7085544b
eduardovale76 <eduardovale76@gmail.com>
2021-02-28 10:14:10 -0300

    Contexto de Template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': 'JkGbB0XtH1w'}
+    return render(request, 'aperitivos/video.html', context={'video': video})
