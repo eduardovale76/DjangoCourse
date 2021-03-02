92cec74506c99b86ab85a623832ee8d0516a213f
eduardovale76 <eduardovale76@gmail.com>
2021-03-01 21:50:49 -0300

    lista de elementos com for

--- pypro/aperitivos/tests/test_indices.py
+++ pypro/aperitivos/tests/test_indices.py
@@ -4,13 +4,20 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    return client.get(reverse('aperitivos:indice', args=('motivacao',)))
+    return client.get(reverse('aperitivos:indice'))
 
 def test_status_code(resp):
     assert resp.status_code==200
 
-#def test_titulo_video(resp):
-#    assert_contains(resp, 'Video Aperitivo: Motivação</h1>')
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video Aperitivo: Motivação',
+        'Instalação Windows'
+    ]
+)
+def test_titulo_video(resp, titulo):
+    assert_contains(resp,titulo)
 
 #def test_conteudo_video(resp):
 #    assert_contains(resp, 'src="https://www.youtube.com/embed/JkGbB0XtH1w"')
